9310b4e50ba03e33207fd35a87079afebbbbf7e2
John Giazlas <yiazlas@gmail.com>
2014-10-25 13:54:56 +0300

    added a test class

---
+++ Git Test/JGTest.h
@@ -0,0 +1,13 @@
+//
+//  JGTest.h
+//  Git Test
+//
+//  Created by John Giazlas on 25/10/14.
+//  Copyright (c) 2014 John Giazlas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JGTest : NSObject
+
+@end
---
+++ Git Test/JGTest.m
@@ -0,0 +1,13 @@
+//
+//  JGTest.m
+//  Git Test
+//
+//  Created by John Giazlas on 25/10/14.
+//  Copyright (c) 2014 John Giazlas. All rights reserved.
+//
+
+#import "JGTest.h"
+
+@implementation JGTest
+
+@end
